7744fd4a5612204afce01163626ccd43e4cb2a69
21garcbe <21garcbe@gmail.com>
2026-02-17 12:59:23 -0500

    added dev branch

--- Lab6_bgarcia15-1.py
+++ Lab6_bgarcia15-1.py
@@ -10,17 +10,18 @@ Each user has a username, real name, password and security level
 # Create a dictionary of users to store user information
 users = {
 
-# Create a dictionary for each user with their username, password, and security level
+# Create a dictionary for each user with key = username, value = another dictionary with keys name, password, and security_level
     'Bgarcia15': {'name': 'Ben', 'password': 'pass1', 'security_level': 'Level 1'},
     'Jdoe': {'name': 'John', 'password': 'pass2', 'security_level': 'Level 1'},
     'Guest': {'name': 'Guest', 'password': 'guest', 'security_level': 'guest'},
     'TitusAndronicus': {'name': 'Titus', 'password': 'shakespeare#1', 'security_level': 'Level 1'}
-
 }
 
 print("Welcome to the login system. Please enter your username and password.")
+
 # Prompt the user to enter their username and check if valid
-username = input("Username: ")
+username = input("Username: ").title()
+
 #set password attempt iterator to 0
 password_attempts = 0
 if username in users:
